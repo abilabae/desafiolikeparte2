b554a10d0587eb20eb50b47057089c7b12c8c1c3
Ana Belen <abilabae@gmail.com>
2023-06-23 18:49:30 -0400

    desafio like parte II

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app= express()
+const cors= require("cors")
+const routes=require("./routes")
+
+const PORT=process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+app.use('/', routes)
+
+
+app.listen(PORT, console.log(`Servidor funcionando en el puerto http://localhost:${PORT}`))
+
